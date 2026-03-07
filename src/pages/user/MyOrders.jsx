dd3af1279664894bd8f71f4e7e3362ea69fb9b57
addasamit <amit94.developer@gmail.com>
2026-03-07 16:43:34 +0530

    code updated

--- src/pages/user/MyOrders.jsx
+++ src/pages/user/MyOrders.jsx
@@ -1,11 +1,36 @@
+import { useEffect, useState } from "react";
 import { FaEye } from "react-icons/fa";
+import { Link } from "react-router-dom";
 import UserSidebar from "../../components/user/UserSidebar";
+import { getAllOrdersService } from "../../services/UserService";
 
 const MyOrders = () => {
-  const orders = [
-    { id: 1, product: "T-Shirt", price: 500, status: "Delivered" },
-    { id: 2, product: "Shoes", price: 2000, status: "Processing" },
-  ];
+
+  const [orders, setOrders] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+      const fetchOrders = async () => {
+        try {
+          const response = await getAllOrdersService();
+          if(response?.status === "success"){
+            setOrders(response?.orders);
+          } else {
+            toast.error(response?.message);
+          }
+        } catch (error) {
+          console.error("Error fetching orders:", error);
+          toast.error(error.response?.data?.message);
+        } finally {
+          setLoading(false);
+        }
+      }
+      fetchOrders();
+    }, []);
+
+  if (loading) {
+    return <p className="text-center mt-5">Loading...</p>;
+  }
 
   return (
     <div className="container mt-5">
@@ -24,46 +49,68 @@ const MyOrders = () => {
                 <th>Product</th>
                 <th>Price</th>
                 <th>Status</th>
+                <th>Order Date</th>
                 <th>View</th>
               </tr>
             </thead>
 
             <tbody>
-              <tr>
-                <td>1</td>
-                <td>T-Shirt</td>
-                <td>₹500</td>
 
-                <td>
+              {orders.length === 0 ? (
+                <tr>
+                  <td colSpan="6" className="text-center">No orders found.</td>
+                </tr>
+              ) : (
+                orders && orders.map((order) => (
+                <tr key={order._id}>
+                  
+                  <td>#{order._id.slice(-6).toUpperCase()}</td>
+
+                  <td>
+                    {order.orderItems?.map((item, index) => (
+                       <div key={item.product?._id || index}>
+                        {item.product?.name || "Product"} (x{item.quantity})
+                      </div>
+                    ))}
+                  </td>
+
+                  <td>₹{order.total}</td>
+
+                  <td>
+                    <span className={`order-status ${
+                      order.orderStatus === "delivered"
+                        ? "status-delivered"
+                        : order.orderStatus === "processing"
+                        ? "status-pending"
+                        : order.orderStatus === "shipped"
+                        ? "status-shipped"
+                        : "status-cancelled"
+                    }`}>
+                      {order.orderStatus}
+                    </span>
+                  </td>
+
+                  <td>{new Date(order.createdAt).toLocaleDateString("en-IN")}</td>
+
+                  <td>
+                    <Link 
+                      to={`/shop-detail/${order.orderItems[0]?.product?._id}`} 
+                      className="view-btn"
+                      target="_blank"
+                    >
+                      <FaEye />
+                    </Link>
+                  </td>
+                </tr>
+
+              ))
+            )}
+                {/* <td>
                   <span className="order-status status-delivered">
                     Delivered
                   </span>
-                </td>
+                </td> */}
 
-                <td>
-                  <button className="view-btn">
-                    <FaEye />
-                  </button>
-                </td>
-              </tr>
-
-              <tr>
-                <td>2</td>
-                <td>Shoes</td>
-                <td>₹2000</td>
-
-                <td>
-                  <span className="order-status status-processing">
-                    Processing
-                  </span>
-                </td>
-
-                <td>
-                  <button className="view-btn">
-                    <FaEye />
-                  </button>
-                </td>
-              </tr>
             </tbody>
           </table>
         </div>
