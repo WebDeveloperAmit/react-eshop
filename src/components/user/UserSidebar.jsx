497f83ef0dc344dcd3d392060b84217812826f65
addasamit <amit94.developer@gmail.com>
2026-03-07 14:22:20 +0530

    code updated

--- src/components/user/UserSidebar.jsx
+++ src/components/user/UserSidebar.jsx
@@ -1,7 +1,41 @@
 import { FaBox, FaHome, FaKey, FaSignOutAlt, FaUser } from "react-icons/fa";
-import { NavLink } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { NavLink, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import { logout } from "../../redux/slices/auth/authSlice";
 
 const UserSidebar = () => {
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You want to logout from your account?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, Logout!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        dispatch(logout());
+        Swal.fire({
+          title: "Logged Out!",
+          text: "You have been logged out.",
+          icon: "success"
+        });
+
+        setTimeout(() => {
+          navigate("/login");
+        }, 1500);
+      }
+    });
+
+  }
+
   return (
     <div className="dashboard-sidebar">
 
@@ -21,9 +55,9 @@ const UserSidebar = () => {
         <FaKey /> Change Password
       </NavLink>
 
-      <NavLink to="/logout">
+      <button onClick={handleLogout} className="logout-btn">
         <FaSignOutAlt /> Logout
-      </NavLink>
+      </button>
 
     </div>
   );
